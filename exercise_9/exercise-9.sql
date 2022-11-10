b14c0e6b33330bbed26467fc9ce3e1d3b3de5140
Connor McCue <connor.mccue@comcast.net>
2022-11-09 18:29:25 -0800

    exercise 9 failing because i swear there are multiple titles that havent been rented

--- exercise_9/exercise-9.sql
+++ exercise_9/exercise-9.sql
@@ -1,3 +1,10 @@
 -- find the film title of all inventory items that have never been rented
 -- LEFT JOIN FROM inventory to film and to rental
 -- use WHERE IS NULL
+
+SELECT film.title
+from film 
+LEFT JOIN inventory on film.film_id = inventory.film_id
+LEFT JOIN rental on inventory.inventory_id = rental.inventory_id
+WHERE 
+  rental.staff_id IS NULL
