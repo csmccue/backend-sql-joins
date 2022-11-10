1d3234c8b2c22218c5ba11fa16621d12a423ee90
Connor McCue <connor.mccue@comcast.net>
2022-11-09 19:56:29 -0800

    exercise 16 no actors

--- exercise_16/exercise_16.sql
+++ exercise_16/exercise_16.sql
@@ -1,3 +1,8 @@
 -- return the title, description, release year and list of actors returned as a JSON blob
 -- for the movie Airplane Sierra
     
+SELECT film.title, film.description, film.release_year
+from film
+WHERE 
+  film.title = 'Airplane Sierra'
+  ;
